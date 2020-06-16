5634be90a9c9308015cdd5e7166414d2aeaaff27
Thomas Woerdeman <t.m.woerdeman@gmail.com>
2020-06-16 16:39:45 +0200

    Initial commit

---
+++ src/test/java/com/twoerdem/prime/PrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twoerdem.prime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
