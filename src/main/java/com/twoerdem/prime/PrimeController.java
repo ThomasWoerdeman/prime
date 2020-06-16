37db774c1141fc0ec32e41b86bae1a62ac5d7165
Thomas Woerdeman <t.m.woerdeman@gmail.com>
2020-06-16 16:54:48 +0200

    isPrime Finished
    isPrime now takes variable and checks if its a prime number

--- src/main/java/com/twoerdem/prime/PrimeController.java
+++ src/main/java/com/twoerdem/prime/PrimeController.java
@@ -1,6 +1,7 @@
 package com.twoerdem.prime;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,7 +11,14 @@ public class PrimeController {
 
 	@RequestMapping(value="/prime/{myInt}", method = RequestMethod.GET)
     @ResponseBody
-	public Boolean isPrime() {
+	public Boolean isPrime(@PathVariable("myInt") Integer myInt) {
+        if (myInt <= 1 || myInt >= 97)
+            return false;
+        for (Integer i = 2; i < myInt; i++) {
+            if (myInt % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
