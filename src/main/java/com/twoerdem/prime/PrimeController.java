147a595e66949b167305ec8a4362557ecc4d8317
Thomas Woerdeman <t.m.woerdeman@gmail.com>
2020-06-16 17:31:42 +0200

    Update PrimeController.java
    Added comments and removed "ResponseBody"

--- src/main/java/com/twoerdem/prime/PrimeController.java
+++ src/main/java/com/twoerdem/prime/PrimeController.java
@@ -4,17 +4,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 public class PrimeController {
 
 	@RequestMapping(value="/prime/{myInt}", method = RequestMethod.GET)
-    @ResponseBody
-	public Boolean isPrime(@PathVariable("myInt") Integer myInt) {
-        if (myInt <= 1 || myInt >= 97)
+	public Boolean isPrime(@PathVariable("myInt") Integer myInt) { //This function checks if myInt is a prime number
+        if (myInt <= 1 || myInt >= 97) //The interger may not be bigger or equal to 97
             return false;
-        for (Integer i = 2; i < myInt; i++) {
+        for (Integer i = 2; i < myInt; i++) { //If the integer is divisible by any number but 1 or itself it is not considered a prime number
             if (myInt % i == 0) {
                 return false;
             }
